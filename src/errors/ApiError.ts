5ab73ee8e50ad0bb51b550523aea3c1a9b1369b9
Chamod Thejan <chamodthejan@192.168.1.8>
2025-07-21 05:59:10 +0530

    add structure

--- src/errors/ApiError.ts
+++
@@ -1,7 +0,0 @@
-// export class APIError extends Error {
-//   status: number
-//   constructor(status: number, message: string) {
-//     super(message)
-//     this.status = status
-//   }
-// }
--- src/routes/notification.routes.ts
+++
@@ -1,9 +0,0 @@
-// import {Router} from "express";
-// import {sendOverdueEmail} from "../controller/notification.controller";
-// import {authenticateToken} from "../middlewares/authentication";
-//
-// const notificationRouter = Router();
-//
-// notificationRouter.post('/send', authenticateToken, sendOverdueEmail);
-//
-// export default notificationRouter;
